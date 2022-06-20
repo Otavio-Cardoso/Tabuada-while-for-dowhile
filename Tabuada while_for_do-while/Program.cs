1a07ffdc85d4ddfbf47a84716c05c37efd8cf6d4
Otávio Cardoso <ocardoso057@gmail.com>
2022-06-19 22:04:39 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada while_for_do-while/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_while_for_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numini = 0, cont = 0, numter = 0, resultado = 0;
+            {
+                //ESTRUTURA FOR
+                Console.WriteLine("Digite a tabuada desejada (método usando for):");
+                numini = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número de inicio desejado: ");
+                cont = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número final desejado: ");
+                numter = int.Parse(Console.ReadLine());
+
+                if (numini <= 0 || cont <= 0 || numter <= 0 || numini > 100 || cont > 100 || numter > 100)
+                {
+                    Console.WriteLine("O número informado excede  o limite");
+                    Console.ReadKey();
+                }
+
+                else
+                {
+                    for (cont = cont; cont <= numter; cont++)
+                    {
+                        resultado = numini * cont;
+                        Console.WriteLine($"{numini} X {cont} = {resultado}");
+                    }
+                }
+                Console.ReadKey();
+
+                //ESTRUTURA DO-WHILE
+                Console.WriteLine("Digite a tabuada desejada (método usando do-while):");
+                numini = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número de inicio desejado: ");
+                cont = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número final desejado: ");
+                numter = int.Parse(Console.ReadLine());
+
+                if (numini <= 0 || cont <= 0 || numter <= 0 || numini > 100 || cont > 100 || numter > 100)
+                {
+                    Console.WriteLine("O número informado excede  o limite");
+                    Console.ReadKey();
+                }
+
+                else
+                {
+                    do
+                    {
+                        resultado = numini * cont;
+                        Console.WriteLine($"{numini} X {cont} = {resultado}");
+                        cont++;
+                    }
+                    while (cont <= numter);
+                }
+                Console.ReadKey();
+
+
+
+                //ESTRUTURA WHILE
+                Console.WriteLine("Digite a tabuada desejada (método usando while):");
+                numini = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número de inicio desejado: ");
+                cont = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número final desejado: ");
+                numter = int.Parse(Console.ReadLine());
+
+                if (numini <= 0 || cont <= 0 || numter <= 0 || numini > 100 || cont > 100 || numter > 100)
+                {
+                    Console.WriteLine("O número informado excede  o limite");
+                    Console.ReadKey();
+                }
+
+                else 
+                {
+                    while (numini <= 10)
+                    {
+                        while (cont <= numter)
+                        {
+                            resultado = numini * cont;
+                            Console.WriteLine($"{numini} X {cont} = {resultado}");
+                            cont++;
+                        }
+                    }
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
